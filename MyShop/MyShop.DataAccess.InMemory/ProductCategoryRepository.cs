9620f2e85de5d0420e95453e820f84551acda5da
Nazim Uddin Asif <nazimuddinasifiiuc@gmail.com>
2020-01-15 18:42:36 +0600

    Add Groping

---
+++ MyShop/MyShop.DataAccess.InMemory/ProductCategoryRepository.cs
@@ -0,0 +1,76 @@
+﻿using MyShop.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Caching;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShop.DataAccess.InMemory
+{
+     public class ProductCategoryRepository
+    {
+        ObjectCache Cache = MemoryCache.Default;
+        List<ProductCategory> ProductCategories = new List<ProductCategory>();
+
+        public ProductCategoryRepository()
+        {
+           ProductCategories = Cache["ProductCategory"] as List<ProductCategory>;
+            if (ProductCategories == null)
+            {
+                ProductCategories = new List<ProductCategory>();
+            }
+        }
+        public void Commit()
+        {
+            Cache["ProductCategory"] = ProductCategories;
+        }
+
+        public void Insert(ProductCategory p)
+        {
+            ProductCategories.Add(p);
+        }
+        public void Update(ProductCategory ProductCategory)
+        {
+            ProductCategory ProductCategoryToUpdate = ProductCategories.Find(p => p.Id == ProductCategory.Id);
+            if (ProductCategoryToUpdate != null)
+            {
+                ProductCategoryToUpdate = ProductCategory;
+            }
+            else
+            {
+                throw new Exception("Product Category No Found");
+            }
+        }
+        public ProductCategory Find(string id)
+        {
+            ProductCategory productCategory = ProductCategories.Find(p => p.Id == id);
+            if (productCategory != null)
+            {
+                return productCategory;
+            }
+            else
+            {
+                throw new Exception("Product Category No Found");
+            }
+        }
+        public IQueryable<ProductCategory> Collection()
+        {
+            return ProductCategories.AsQueryable();
+        }
+
+        public void Delete(string Id)
+        {
+            ProductCategory productCategoryToDelete = ProductCategories.Find(p => p.Id == Id);
+            if (productCategoryToDelete != null)
+            {
+                ProductCategories.Remove(productCategoryToDelete);
+            }
+            else
+            {
+                throw new Exception("Product Category No Found");
+            }
+        }
+    }
+}
+
--- MyShop/MyShop.WebUI/Controllers/ProductManagerController.cs
+++ MyShop/MyShop.WebUI/Controllers/ProductManagerController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using MyShop.Core.Models;
+using MyShop.Core.ViewModels;
 using MyShop.DataAccess.InMemory;
 
 namespace MyShop.WebUI.Controllers
@@ -11,9 +12,11 @@ namespace MyShop.WebUI.Controllers
     public class ProductManagerController : Controller
     {
         ProductRepository context;
+        ProductCategoryRepository productCategories;
         public ProductManagerController()
         {
             context = new ProductRepository();
+            productCategories = new ProductCategoryRepository();
         }
         // GET: ProductManager
         public ActionResult Index()
@@ -25,8 +28,10 @@ namespace MyShop.WebUI.Controllers
 
         public ActionResult Create()
         {
-            Product product = new Product();
-                return View(product);
+            ProductManagerViewModel viewModel = new ProductManagerViewModel();
+            viewModel.Product = new Product();
+            viewModel.ProductCategories = productCategories.Collection();
+                return View(viewModel);
         }
         [HttpPost]
         public ActionResult Create(Product product)
@@ -67,7 +72,10 @@ namespace MyShop.WebUI.Controllers
             {
                 if (!ModelState.IsValid)
                 {
-                    return View(product);
+                    ProductManagerViewModel viewModel = new ProductManagerViewModel();
+                    viewModel.Product = new Product();
+                    viewModel.ProductCategories = productCategories.Collection();
+                    return View(viewModel);
 
                 }
                 productToEdit.Category = product.Category;
