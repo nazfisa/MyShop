3918103827d14390a422bf41de066247f8b6344d
Nazim Uddin Asif <nazimuddinasifiiuc@gmail.com>
2020-01-17 11:24:53 +0600

    Added busket

---
+++ MyShop/MyShop.DataAccess.SQL/Migrations/202001170455478_AddBusket.cs
@@ -0,0 +1,34 @@
+﻿namespace MyShop.DataAccess.SQL.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddBusket : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Baskets",
+                c => new
+                    {
+                        Id = c.String(nullable: false, maxLength: 128),
+                        CreatedAt = c.DateTimeOffset(nullable: false, precision: 7),
+                        BasketId = c.String(maxLength: 128),
+                        ProductId = c.String(),
+                        Quantity = c.String(),
+                        Discriminator = c.String(nullable: false, maxLength: 128),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Baskets", t => t.BasketId)
+                .Index(t => t.BasketId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Baskets", "BasketId", "dbo.Baskets");
+            DropIndex("dbo.Baskets", new[] { "BasketId" });
+            DropTable("dbo.Baskets");
+        }
+    }
+}
